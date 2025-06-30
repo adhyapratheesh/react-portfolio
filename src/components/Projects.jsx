5e8c680836708adbc5481db9926d2d9c0a2e54d8
Adhya Pratheesh <adhyapratheesh@gmail.com>
2025-06-30 18:34:25 +0530

    Added Projects Section

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import './Projects.css'; 
 
-const Projects = () => {
-  return (
-    <div>
-      Projects
-    </div>
-  )
-}
+const Projects = () => (
+  <section id="projects" className="projects">
+    <h2>Projects</h2>
+    <div className="project-container">
+      <div className="project-card">
+          <h3>Travel Guide Website</h3>
+          <p>A personalized Kerala travel planner with filters and smart itineraries using React and Gemini API.</p>
+      </div>
 
-export default Projects
+      <div className="project-card">
+          <h3>SkillSync</h3>
+          <p>Automated team formation platform using Django, PostgreSQL, and ML for smart project allocation.</p>
+        </div>
+
+      <div className="project-card">
+          <h3>Movie Recommendation System</h3>
+          <p>An ML-based movie recommender using content filtering and user preferences.</p>
+        </div>
+        
+      </div>
+  </section>
+);
+
+export default Projects;
